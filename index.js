2c4f6b3c23dcd69b301554984a01ff78e16c1ec3
Giacomo Zinetti <giacomo.zinetti@objectway.it>
2016-07-18 17:32:16 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// jshint esversion:6
+
+const fs = require('fs');
+const path = require('path');
+const argv = require('yargs').argv;
+const benchmark = require('benchmark');
+const inquirer = require('inquirer');
+
+const runs = {
+  benchmarkjs: function(mod) {
+    const suite = new benchmark.Suite();
+
+    for (let method in mod) {
+      if (mod.hasOwnProperty(method) && typeof mod[method] === "function") {
+        suite.add(method, mod[method]);
+      }
+    }
+
+    suite
+      .on('start', () => console.log('Starting tests'))
+      .on('cycle', event => console.log(String(event.target)))
+      .on('complete', function() {
+        console.log(`Fastest: ${this.filter('fastest').map('name')}`);
+      })
+      .run({ 'async': true });
+  },
+  bench: function(mod) {
+    exports.compare = mod;
+    require("bench").runMain();
+  },
+};
+
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'tester',
+    message: 'Choose the tester that you want to use:',
+    choices: ['bench', 'benchmarkjs'],
+  },
+  {
+    type: 'list',
+    name: 'module',
+    message: 'Which module do you want to test:',
+    choices: () => fs.readdirSync(path.join(__dirname, 'test')),
+  },
+]).then(function (answers) {
+  const mod = require(path.join(__dirname, 'test', answers.module));
+  runs[answers.tester](mod);
+});
---
+++ test/example.js
@@ -0,0 +1,6 @@
+/* jshint esversion:6 */
+
+module.exports = {
+  f1: () => true,
+  f2: () => false,
+};
